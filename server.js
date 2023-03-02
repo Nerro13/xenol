25b7b5c377f0d3b4412c40e47ef34726f530bf75
Nerro <nduguiskarioti@gmail.com>
2023-03-02 13:31:42 +0300

    cont

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
-const exp = require("express")
-const server = exp()
-server.set("view engne", "ejs")
-server.get("/", (req, res)=>{
-    res.render("home")
-})
-server.get("/login", (req, res)=>{
-    res.render("login")
-})
-server.get("/register", (req, res)=>{
-    res.render("register")
-})
-server.get("/about", (req, res)=>{
-    res.render("here we go")
-})
+const exp = require("express");
+const server = exp();
+server.set("view engine", "ejs");
+server.use(exp.static("public"));
+server.get("/", (req, res) => {
+  res.render("home");
+});
+server.get("/login", (req, res) => {
+  res.render("login");
+});
+server.get("/register", (req, res) => {
+  res.render("register");
+});
+server.get("/about", (req, res) => {
+  res.render("about");
+});
 server.listen(3000, () => {
-    console.log("server listening on port 3000")
-})
+  console.log("server listening on port 3000");
+});
